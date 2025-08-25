f021b1c6477300fb0e1177f49356d726f07ed2ae
Parth Bhardwaj <pbhardwaj@datopic.com>
2025-08-25 23:37:35 +0530

    set up

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -12,6 +13,7 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      'eslintPlugginSimpleImportSort'
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -22,8 +24,15 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    plugins:{
+    'simple-import-sort':simpleImportSort
+    },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      "semi":["error","always"],
+      "quotes":["error","single"],
+      "simple-import-sort/imports":"error",
+      "simple-import-sort/export":"error"
     },
   },
 ])
