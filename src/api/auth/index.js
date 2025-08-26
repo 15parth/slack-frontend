dff32aa46c469f0bb7f28197811121edf98e8392
Parth Bhardwaj <pbhardwaj@datopic.com>
2025-08-26 08:32:03 +0530

    react query setup

---
+++ src/api/auth/index.js
@@ -0,0 +1,30 @@
+import axios from '@config/axiosConfig';
+
+export const signUpRequest = async ({email,password,username}) =>{
+    try {
+        const response= axios.post("/user/signup",{
+            email,
+            password,
+            username
+        });
+
+        return response?.data;
+    } catch (error) {
+      console.error(error);
+       throw error.response.data;
+    }
+}
+
+export const signInRequest= async ({email, password})=>{
+    try {
+         const response= axios.post("/users/signin",{
+            email,
+            password,
+        });
+
+        return response?.data;
+    } catch (error) {
+        console.error(error);
+        throw error.response.data;
+    }
+}
