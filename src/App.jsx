e004234976d0461ab125664a930ae2f8ef5bf5d7
Parth Bhardwaj <pbhardwaj@datopic.com>
2025-08-31 16:20:10 +0530

    sign up flow changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,22 @@ import { Auth } from './pages/Auth/Auth'
 import SignupCard from '@/components/organisms/Auth/SignupCard'
 import SignInCard from '@/components/organisms/Auth/SignInCard'
 import Notfound from '@/components/organisms/Notfound/Notfound'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import SignupContainer from '@/components/organisms/Auth/SignupContainer'
 
 function App() {
 
+  const queryClient= new QueryClient();
+
+
   return (
+    <QueryClientProvider client={queryClient}>
     <Routes>
-      <Route path="/auth/signup" element={<Auth> <SignupCard/></Auth>}/>
+      <Route path="/auth/signup" element={<Auth> <SignupContainer/></Auth>}/>
       <Route path="/auth/signin" element={<Auth> <SignInCard/></Auth>}/>
       <Route path="/*" element={<Notfound/> }/>
     </Routes>
+    </QueryClientProvider>
   )
 }
 
